58ee45f273d3115b6314f86cff6b5d7d326d1241
Alexshch09 <91371397+Alexshch09@users.noreply.github.com>
2024-09-17 09:25:11 +0000

    php

--- src/wedkarstwo/zgloszenia.php
+++ src/wedkarstwo/zgloszenia.php
@@ -4,5 +4,10 @@ $lowisko = $_POST["lowisko"];
 $data = $_POST["data"];
 $sedzia = $_POST["sedzia"];
 
-$conn = mysqli_connect("localhost", "root", "root", "wedkarstwo");
+$conn = mysqli_connect("localhost", "root", "", "wedkarstwo");
 
+$sql = "INSERT INTO `zawody_wedkarskie`(`Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (0, $lowisko, '$data', '$sedzia')";
+
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
